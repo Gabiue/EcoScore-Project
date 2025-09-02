55a1f0e3b7a447178ef831e9b4b8bfa4f2bc69f3
Gabriel Kaue <gabrielkauerodriguest@gmail.com>
2025-09-02 11:36:04 -0300

    first commit

---
+++ src/main/java/com/ucbprojeto/ecoscore/EcoscoreApplication.java
@@ -0,0 +1,13 @@
+package com.ucbprojeto.ecoscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoscoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucbprojeto/ecoscore/EcoscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucbprojeto.ecoscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
