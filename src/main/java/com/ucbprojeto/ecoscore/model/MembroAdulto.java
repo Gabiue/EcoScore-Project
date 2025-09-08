5d8884bd4773d41919bc4f360a24a0796689b84f
Geovane Saraiva da Silva <70657835+0LostConnection@users.noreply.github.com>
2025-09-08 20:53:45 -0300

    feat: adiciona coluna "eh_responsavel" à MembroAdulto

--- src/main/java/com/ucbprojeto/ecoscore/model/MembroAdulto.java
+++ src/main/java/com/ucbprojeto/ecoscore/model/MembroAdulto.java
@@ -3,6 +3,7 @@ package com.ucbprojeto.ecoscore.model;
 public class MembroAdulto {
     private String cpf;
     private String email;
+    private boolean eh_responsavel;
 
     public String getCpf() {
         return cpf;
@@ -18,5 +19,13 @@ public class MembroAdulto {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public boolean isEh_responsavel() {
+        return eh_responsavel;
+    }
+
+    public void setEh_responsavel(boolean eh_responsavel) {
+        this.eh_responsavel = eh_responsavel;
     }
 }
