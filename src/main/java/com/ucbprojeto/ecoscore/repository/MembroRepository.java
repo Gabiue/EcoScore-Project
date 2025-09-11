7381610e961f642da41fff85c3ce76c9a23d1709
Seu Nome <gabrielkauerodriguest@gmail.con>
2025-09-11 20:05:32 -0300

    feat: implementa MembroRepository

---
+++ src/main/java/com/ucbprojeto/ecoscore/repository/MembroRepository.java
@@ -0,0 +1,53 @@
+package com.ucbprojeto.ecoscore.repository;
+
+import com.ucbprojeto.ecoscore.model.Membro;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class MembroRepository  {
+    private final JdbcTemplate jdbcTemplate;
+
+    private final RowMapper<Membro> rowMapper = (rs, rowNum) -> {
+        Membro membro = new Membro();
+        membro.setCpf(rs.getString("cpf"));
+        membro.setId_familia(rs.getInt("id_familia"));
+        membro.setNome(rs.getString("nome"));
+        membro.setData_nascimento(rs.getDate("data_nascimento"));
+        membro.setPapel_familia(rs.getString("papel_familia"));
+        membro.setCpf_supervisor(rs.getString("cpf_supervisor"));
+        return membro;
+    };
+
+
+    public MembroRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Membro> findAll() {
+        String sql = "SELECT * FROM membro";
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+
+    public Membro findById(String cpf) {
+        String sql = "SELECT * FROM membro WHERE cpf = ?";
+        return jdbcTemplate.queryForObject(sql, rowMapper, cpf);
+    }
+    public void save(Membro membro) {
+        String sql = "INSERT INTO membro (cpf, id_familia, nome, data_nascimento, papel_familia, cpf_supervisor) VALUES (?, ?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql, membro.getCpf(), membro.getId_familia(), membro.getNome(), membro.getData_nascimento(), membro.getPapel_familia(), membro.getCpf_supervisor());
+    }
+    public void update(Membro membro) {
+        String sql = "UPDATE membro SET id_familia = ?, nome = ?, data_nascimento = ?, papel_familia = ?, cpf_supervisor = ? WHERE cpf = ?";
+        jdbcTemplate.update(sql, membro.getId_familia(), membro.getNome(), membro.getData_nascimento(), membro.getPapel_familia(), membro.getCpf_supervisor(), membro.getCpf());
+    }
+    public void delete(String cpf) {
+        String sql = "DELETE FROM membro WHERE cpf = ?";
+        jdbcTemplate.update(sql, cpf);
+    }
+
+
+}
